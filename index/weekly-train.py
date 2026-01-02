33d85c9d30dd5ed66865ec8db5c87f1796e98aee
raymondso123 <blotzytgaming@gmail.com>
2026-01-02 16:36:33 -0500

    update train

--- index/weekly-train.py
+++ index/weekly-train.py
@@ -21,7 +21,7 @@ symbols = {
     "JPM", "BAC", "V", "BRK-B", "PYPL", "GS", "MS", "WFC", "TD", "RY", "AXP", "AIG", "SCHW", "BTC-USD", "ETH-USD",
     "KO", "PG", "WMT", "MCD", "DG", "DE", "HON", "MMM", "DOW", "FCX", "LIN", "COST", "HD", "LOW", "TGT", "SBUX", "NKE",
     "JNJ", "UNH", "PFE", "RY.TO", "TD.TO", "BNS.TO", "BMO.TO", "CM.TO", "ENB.TO", "CNQ.TO", "SU.TO", "TRP.TO", "ABX.TO", "WPM.TO", "CP.TO", "CNR.TO", "BCE.TO", "T.TO",
-    "XOM", "CVX", "CAT", "GE", "ENB", "H.TO", "CU.TO", "SLB", "HAL", "DVN", "PXD", "COP",
+    "XOM", "CVX", "CAT", "GE", "ENB", "H.TO", "CU.TO", "SLB", "HAL", "DVN", "COP",
     "NEE", "O", "NEM", "TLT", "IEF", "HYG", "LQD",
     "SPY", "QQQ", "IWM", "XLF", "XLE", "ARKK",
     "TNA", "TZA", "ROKU", "SOFI", 
