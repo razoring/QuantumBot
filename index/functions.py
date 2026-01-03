4d23755c6b1193a5dd9edfe67747e9819f56ea06
raymondso123 <blotzytgaming@gmail.com>
2026-01-03 02:39:56 -0500

    adjusting

--- index/functions.py
+++ index/functions.py
@@ -196,10 +196,10 @@ class Charts:
         self._thread = threading.Lock()
         self._ttl = 60*60*24 # 60 seconds = 60 minutes = 24 hours before expiry
         self._capacity = 64
-        self._inflections = 25
-        self._scale = 0.5
-        self._confidence = 0.2
-        self._samples = 2000
+        self._inflections = 10
+        self._scale = 0.01
+        self._confidence = 0.8
+        self._samples = 2500
 
     def _impliedVolatility(self, stock, lastDate, forward, curPrice, quantiles, futureDays):
         anchorsY = [[curPrice] * len(quantiles)] 
@@ -467,7 +467,7 @@ class Charts:
                 data = window.reset_index()[["Date", "Close"]].rename(columns={"Date": "ds", "Close": "y"})
                 data["ds"] = data["ds"].dt.tz_localize(None)
 
-                config = ph(daily_seasonality=False, yearly_seasonality=True, weekly_seasonality=True, n_changepoints=self._inflections, changepoint_prior_scale=self._scale, changepoint_range=self._confidence, uncertainty_samples=self._samples)
+                config = ph(daily_seasonality=False, yearly_seasonality=False, weekly_seasonality=True, n_changepoints=self._inflections, changepoint_prior_scale=self._scale, changepoint_range=self._confidence, uncertainty_samples=self._samples)
                 
                 config.fit(data)
                 future = config.make_future_dataframe(periods=91, freq=settings[1]) 
@@ -534,7 +534,7 @@ class Charts:
                 data = window.reset_index()[["Date", "Close"]].rename(columns={"Date": "ds", "Close": "y"})
                 data["ds"] = data["ds"].dt.tz_localize(None)
 
-                config = ph(daily_seasonality=False, yearly_seasonality=True, weekly_seasonality=True, n_changepoints=self._inflections, changepoint_prior_scale=self._scale, changepoint_range=self._confidence, uncertainty_samples=self._samples) # cpps = 0.05
+                config = ph(daily_seasonality=True, yearly_seasonality=True, weekly_seasonality=True, seasonality_prior_scale=50, n_changepoints=self._inflections, changepoint_prior_scale=self._scale, changepoint_range=self._confidence, uncertainty_samples=self._samples) # cpps = 0.05
                 config.fit(data)
 
                 future = config.make_future_dataframe(periods=forward, freq=nested[1])
