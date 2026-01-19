e14230613b4a435f3d339eb0d2e83dba1ab56ecf
raymondso123 <blotzytgaming@gmail.com>
2026-01-19 02:19:08 -0500

    add prod sql db

---
+++ api/QuantumBotDB-Prod.sql
@@ -0,0 +1,56 @@
+create table Account(
+	id bigserial primary key,
+	email text unique ,
+	phone text,
+	discord text unique,
+	premium boolean default false,
+	exchange text default 'US',
+	preferences jsonb,
+	credits bigint default 0,
+	created timestamptz default now(),
+	updated timestamptz default now()
+);
+
+create table Sensitives(
+	id bigserial primary key,
+	account bigint references Account(id) unique not null,
+	country text,
+	birthday text,
+	gender text,
+	race text,
+	updated timestamptz default now()
+);
+
+create table Alert (
+	id bigserial primary key,
+	account bigint references Account(id) not null,
+	symbol text,
+	price double precision,
+	updated timestamptz default now()
+);
+
+create table Ticker(
+	id bigserial primary key,
+	ticker text unique,
+	sector text,
+	industry text,
+	active boolean default true,
+	accuracy double precision,
+	weight jsonb,
+	datapoints jsonb,
+	updated timestamptz default now()
+);
+
+create table Request (
+	id bigserial primary key,
+	ticker bigint references Ticker(id) not null, 
+	account bigint references Account(id) not null,
+	updated timestamptz default now()
+);
+
+create index RequestAnalytics on Request(account, ticker);
+create index AccountLogins on Account(discord, email);
+create index AlertsAccount on Alert(account);
+create index TickerActive on Ticker(ticker) where active = true;
+
+
