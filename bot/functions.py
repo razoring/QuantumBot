008d0e702141c7b5cd4d85c6f45ff624b69a8b4f
raymondso123 <blotzytgaming@gmail.com>
2026-01-26 23:17:57 -0500

    live train with database connection

--- bot/functions.py
+++ bot/functions.py
@@ -361,7 +361,9 @@ class Charts:
             avgInd = [prevInd[j]*(1-adjustment) + bestWeight[j]*adjustment for j in range(len(prevInd))] #ema
             weights = [avgInd,countInd+1]
             print(origin.date(), bestError, str(round(adjustment*100,2))+"%", bestWeight)
-        cursor.execute(f"update ticker set weight = '{weights}' where ticker = '{ticker}';")
+        cursor.execute(f"update ticker set weight = '{json.dumps(weights)}' where ticker = '{ticker}';")
+        connection.commit()
+        cursor.close()
 
     def project(self, ticker, model, serverName, serverInvite, serverIcon):
         forward = 90
